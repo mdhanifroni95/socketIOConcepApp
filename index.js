2ae0dd6d0acc33285031f3cd51e691f5cf4228f2
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-10-15 23:05:53 +0600

    socket io implement

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const { Server } = require("socket.io");
 
 //create server
 const expressServer = http.createServer(app);
+const io = new Server(expressServer);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log(`a user connected`);
+  socket.on("disconnect", () => {
+    console.log(`user disconnected`);
+  });
+});
+
 expressServer.listen(3000, () => {
-  console.log("server run at 300");
+  console.log("server run at 3000");
 });
